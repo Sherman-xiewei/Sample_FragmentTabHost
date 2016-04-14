0db906f8ea10908b3cf97576144ae4eb00db145b
Sherman-xiewei <xie.wei@inventec.com>
2016-04-14 14:57:38 +0800

    upload project

---
+++ app/src/main/java/inventec/icc/sample_fragmenttabhost/MainActivity.java
@@ -0,0 +1,55 @@
+package inventec.icc.sample_fragmenttabhost;
+
+import android.support.v4.app.FragmentTabHost;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TabHost;
+import android.widget.TextView;
+
+import inventec.icc.sample_fragmenttabhost.fragment.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FragmentTabHost mTabHost;
+    private LayoutInflater mLayoutInflater;
+
+    private Class mFragmentArray[] = {Fragment1.class, Fragment2.class, Fragment3.class, Fragment4.class};
+    private int mImageArray[] = {R.mipmap.ic_launcher, R.mipmap.ic_launcher, R.mipmap.ic_launcher, R.mipmap.ic_launcher};
+    private String mTextArray[] = {"首页", "查询", "消息", "我的"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        mLayoutInflater = LayoutInflater.from(this);
+
+        // 找到TabHost
+        mTabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
+        mTabHost.setup(this, getSupportFragmentManager(), R.id.realtabcontent);
+        // 得到fragment的个数
+        int count = mFragmentArray.length;
+        for (int i = 0; i < count; i++) {
+            // 给每个Tab按钮设置图标、文字和内容
+            TabHost.TabSpec tabSpec = mTabHost.newTabSpec(mTextArray[i]).setIndicator(getTabItemView(i));
+            // 将Tab按钮添加进Tab选项卡中
+            mTabHost.addTab(tabSpec, mFragmentArray[i], null);
+        }
+    }
+
+    private View getTabItemView(int index) {
+        View view = mLayoutInflater.inflate(R.layout.tab_item_view, null);
+        ImageView imageView = (ImageView) view.findViewById(R.id.imageview);
+        imageView.setImageResource(mImageArray[index]);
+        TextView textView = (TextView) view.findViewById(R.id.textview);
+        textView.setText(mTextArray[index]);
+
+        return view;
+    }
+}
